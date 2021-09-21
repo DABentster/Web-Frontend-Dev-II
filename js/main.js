daa82623131161f00b74a087a9aa7ee0803220ab
DABentster <90743655+DABentster@users.noreply.github.com>
2021-09-20 22:02:53 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
+//Table of Contents - List of Weekly Activities and/or Notes
 const links = [
-  { label: 'Week 1 Notes', url: 'week1.html', }, // you can add other tags if you like
-  { label: 'Week 2 Notes', url: 'week2.html', },
-  { label: 'Week 3 Notes', url: 'week3.html', },
+  { label: 'Assignment Week 01: Test', url: 'test.html', },
+  { label: 'Assignment Week 02: Adding Machine', url: 'week2.html', },
+  { label: 'Assignment Week ##: NAME', url: 'week##.html', },
 ];
 
-const ol = document.getElementById('assignments');      // whatever your OL tag ID is
+const ol = document.getElementById('table_of_contents');
 links.forEach(link => {
   let li = document.createElement('li');
   let a = document.createElement('a');
   a.setAttribute('href', link.url);
   a.innerText = link.label;
-  // add other tags of interest if you like (date submitted, score)
   li.appendChild(a);
   ol.appendChild(li);
 });
