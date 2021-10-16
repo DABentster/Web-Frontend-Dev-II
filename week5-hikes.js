04906e1512fac60c50197fc9fb9ecbe847afd069
DABentster <dabentster@gmail.com>
2021-10-16 17:06:35 -0600

    Week5

--- week5-hikes.js
+++ week5-hikes.js
@@ -76,7 +76,7 @@ export default class Hikes {
     // in order to show the details of a hike ontouchend we will need to attach a listener AFTER the list of hikes has been built. The function below does that.
     addHikeListener() {
         // We need to loop through the children of our list and attach a listener to each, remember though that children is a nodeList...not an array. So in order to use something like a forEach we need to convert it to an array.
-        const list = Array.from(this.parentElement);
+        let list = Array.from(this.parentElement);
         list.forEach(listItem => {
             listItem.addEventListener("click", this.showOneHike(listItem.id));
         });
