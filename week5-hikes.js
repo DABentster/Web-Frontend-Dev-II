0f192aa42e3641aaf807cd4f676f40d1d7308821
DABentster <dabentster@gmail.com>
2021-10-16 17:28:36 -0600

    Update week5-hikes.js

--- week5-hikes.js
+++ week5-hikes.js
@@ -70,7 +70,7 @@ export default class Hikes {
     showOneHike(hikeName) {
         this.parentElement.innerHTML = "";
         this.parentElement.appendChild(renderOneHikeFull(this.getHikeByName(hikeName)));
-        this.parentElement.appendChild(buildBackButton());
+        this.parentElement.appendChild(this.buildBackButton());
     }
 
     // in order to show the details of a hike ontouchend we will need to attach a listener AFTER the list of hikes has been built. The function below does that.
